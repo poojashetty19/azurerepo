79014b42cd76d1c548c994baf9dbc818d56f14b2
poojashetty19 <jpoojashetty@gmail.com>
2023-03-12 16:18:12 +0530

    Create main.tf

---
+++ 3tier/modules/virtualmachine/main.tf
@@ -0,0 +1,85 @@
+resource "azurerm_network_interface" "app-interface" {
+  name                = "app-nic"
+  location            = var.rglocation
+  resource_group_name = var.rgname
+
+  ip_configuration {
+    name                          = "app-ip"
+    subnet_id                     = var.app_subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+
+resource "azurerm_windows_virtual_machine" "appnewvm" {
+  name                  = "app-vm"
+  admin_username        = "azureuser"
+  admin_password        = random_password.password.result
+  location              = azurerm_resource_group.azure-rg.location
+  resource_group_name   = azurerm_resource_group.azure-rg.rg.name
+  size                  = "Standard_DS1_v2"
+
+  os_disk {
+    name                 = "myOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter-azure-edition"
+    version   = "latest"
+  }
+  
+  resource "azurerm_network_interface" "web-interface" {
+  name                = "web-nic"
+  location            = var.rglocation
+  resource_group_name = var.rgname
+
+  ip_configuration {
+    name                          = "web-ip"
+    subnet_id                     = var.web_subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+
+resource "azurerm_windows_virtual_machine" "webnewvm" {
+  name                  = "web-vm"
+  admin_username        = "azureuser"
+  admin_password        = random_password.password.result
+  location              = azurerm_resource_group.azure-rg.location
+  resource_group_name   = azurerm_resource_group.azure-rg.rg.name
+  size                  = "Standard_DS1_v2"
+
+  os_disk {
+    name                 = "myOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter-azure-edition"
+    version   = "latest"
+  }
+  
+  resource "random_id" "random_id" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = azurerm_resource_group.rg.name
+  }
+
+  byte_length = 8
+}
+
+resource "random_password" "password" {
+  length      = 20
+  min_lower   = 1
+  min_upper   = 1
+  min_numeric = 1
+  min_special = 1
+  special     = true
+}
