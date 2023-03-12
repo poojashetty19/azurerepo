7a32f064de3d4235d5ab561e514375f2918c4249
poojashetty19 <jpoojashetty@gmail.com>
2023-03-12 16:46:01 +0530

    Create outputs.tf

---
+++ 3tier/modules/network/outputs.tf
@@ -0,0 +1,15 @@
+output "network_name" {
+  value = azurerm_virtual_network.vnet1.name
+}
+
+output "websubnet_id" {
+  value = azurerm_subnet.web-subnet.id
+}
+
+output "appsubnet_id" {
+  value = azurerm_subnet.app-subnet.id
+}
+
+output "dbsubnet_id" {
+  value = azurerm_subnet.db-subnet.id
+}
