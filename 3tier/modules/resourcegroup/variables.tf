2316ff54256eb806c45a0d781bf23d9e3b9c3a6d
poojashetty19 <jpoojashetty@gmail.com>
2023-03-12 15:29:40 +0530

    Create variables.tf

---
+++ 3tier/modules/resourcegroup/variables.tf
@@ -0,0 +1,9 @@
+variable "rgname" {
+    type = string
+    description = "resource group name"    
+}
+
+variable "rglocation" {
+    type = string
+    description = "resource group location"    
+}
