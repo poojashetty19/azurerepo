236606c8c9a42447154c505672c1958dfbb2a876
poojashetty19 <jpoojashetty@gmail.com>
2023-03-12 17:15:38 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "rgname" {}
+variable "rglocation" {}
+variable "database_server" {}
+variable "database_name" {}
+variable "database_password" {}
+variable "database_version" {}
+variable "vm_size" {}
+variable "vm_sku" {}
+variable "publisher" {}
+variable "websubnet" {}
+variable "appsubnet" {}
+variable "dbsubnet" {}
+variable "vnetip" {}
+ 
+  
+
